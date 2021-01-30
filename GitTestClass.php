ce1b7ae231c20a96ecb2d51f0bb533baa0cd5d03
jongchan988 <jongchan988@gmail.com>
2021-01-30 23:20:55 +0900

    aaaaaa

--- GitTestClass.php
+++ GitTestClass.php
@@ -3,10 +3,7 @@ class GitTestClass{
     private  $test;
     public function __construct($array)
     {
-        $default = [];
-        $array = array_push($default, $array);
-        $this->test = $array;
-        $this->setUp();
+
     }
     private function setUp(){
         echo "setUp";
