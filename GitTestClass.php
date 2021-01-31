436b9b73376c8a7f0087248e36da2104f5b4064e
jongchan988 <jongchan988@gmail.com>
2021-01-31 09:41:38 +0900

    another commit finsish3 setup

--- GitTestClass.php
+++ GitTestClass.php
@@ -9,6 +9,7 @@ class GitTestClass{
         $this->setUp();
         echo "fnish";
         echo "add finish 2";
+	echo "add finish 3 in another place"
     }
     private function setUp(){
         echo "setUp";
